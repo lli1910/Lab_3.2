cab3fed289d6cd3da7841a261d20d321caee59c3
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-02-07 09:23:20 -0500

    yoyoy

--- Lab_3.2/src/MethodArrays2.java
+++ Lab_3.2/src/MethodArrays2.java
@@ -4,20 +4,33 @@
 //Merge & Partition Sorts
 public class MethodArrays2 {
 	public static void main (String args[]) {
-		String[] list1 = {};
-	
+		String[] list1 = {"Alpha","Charlie","Echo"}, list2 = {"Beta","Delta"};
+		
+		for(String s: merge(list1,list2)) {
+			System.out.println(s);
+		}
+		System.out.println("YOO");
+		
 	}
 	
 	public static String[] merge (String[] list1, String[] list2) {
-		int diff = list1.length - list2.length;
+		int diff = list1.length - list2.length, newIndex = 0;
 		String[] newArr = {};
 		for(int i = 0; i < list1.length + diff; i++) {
-			for(int j = i; j < list2.length + diff; j++) {
-				if(list1[i] < list2[j]) {
-					newArr += list1[i];
+			for(int j = i; j < list2.length+diff; j++) {
+				if(list1[i].compareTo(list2[j])>0) {
+					newArr[newIndex] = list1[i];
+					newIndex++;
+					i++;
+					j--;
+				}
+				else if(list2[j].compareTo(list1[i])>0) {
+					newArr[newIndex] = list2[j];
+					newIndex++;
 				}
 			}
 		}
+		return newArr;
 	}
 	
 	//public static int partition (int[] list) {
